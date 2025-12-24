0cd49205ceb1ee99d6461db8b2e64f5815c21d0f
geeky-lynx <markomacura003@gmail.com>
2025-12-25 00:39:36 +0100

    logging-service: Update the logging service interface

--- back/microservices/logging-microservice/src/Domain/services/ILoggingService.ts
+++ back/microservices/logging-microservice/src/Domain/services/ILoggingService.ts
@@ -1,3 +1,23 @@
+import { ResultAsync } from "neverthrow";
+import { LoggingError } from "../enums/LoggingError";
+import { LogDTO } from "../DTOs/LogDTO"
+import { LoggingKind } from "../enums/LoggingKind";
+
+export type LogFilterParams = {
+  kinds?: LoggingKind[],
+  dateStart?: Date,
+  dateEnd?: Date,
+  timeStart?: Date,
+  timeEnd?: Date,
+  descContains?: string,
+  descLike?: string
+}
+
 export interface ILoggingService {
-  //
+  store(log: LogDTO): Promise<ResultAsync<undefined, LoggingError>>;
+  read(logId: number): Promise<ResultAsync<LogDTO, LoggingError>>;
+  update(log: LogDTO): Promise<ResultAsync<LogDTO, LoggingError>>;
+  delete(logId: number): Promise<ResultAsync<undefined, LoggingError>>;
+  // find(): Promise<ResultAsync<LogDTO, LoggingError>>;
+  filter(params: LogFilterParams): Promise<ResultAsync<LogDTO[], LoggingError>>;
 }
