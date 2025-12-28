3ee80c1dc7711cf84731800d21d35df05455e834
geeky-lynx <markomacura003@gmail.com>
2025-12-28 21:53:47 +0100

    logging-microservice: Fix failure on database initialization

--- back/microservices/logging-microservice/src/Database/InitializeConnection.ts
+++ back/microservices/logging-microservice/src/Database/InitializeConnection.ts
@@ -1,16 +1,28 @@
-import { fromAsyncThrowable } from "neverthrow";
+import { fromAsyncThrowable, okAsync, errAsync } from "neverthrow";
 import { Db } from "./DbConnectionPool";
 
 
 
 export async function initializeDatabase() {
-  const result = fromAsyncThrowable(Db.initialize);
+  const result = await wrappedInitialize();
   
-  result().match(
-    (ds: object) => { console.log("\x1b[34m[DbConn@1.12.4]\x1b[0m Database connected"); },
+  result.match(
+    (ds: void) => { console.log("\x1b[34m[DbConn@1.12.4]\x1b[0m Database connected"); },
     (err: any) => {
       console.error("\x1b[31m[DbConn@1.12.4]\x1b[0m Error during DataSource initialization (Read below):");
       console.error(err);
     }
   );
+}
+
+
+
+async function wrappedInitialize() {
+  try {
+    await Db.initialize();
+    return okAsync();
+  }
+  catch (err) {
+    return errAsync(err);
+  }
 }
