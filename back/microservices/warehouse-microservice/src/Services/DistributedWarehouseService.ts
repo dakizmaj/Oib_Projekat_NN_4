30b24114bceed80c06881829f45bf9e14f126e57
geeky-lynx <markomacura003@gmail.com>
2025-12-29 00:06:59 +0100

    [UNIMPLEMENTED] warehouse-microservice: Add 2 service classes

---
+++ back/microservices/warehouse-microservice/src/Services/DistributedWarehouseService.ts
@@ -0,0 +1,11 @@
+import { ResultAsync, errAsync } from "neverthrow";
+import { PackagingDTO } from "../Domain/DTOs/PackagingDTO";
+import { IWarehouseService } from "../Domain/services/IWarehouseService";
+
+
+
+export class DistributedWarehouseService implements IWarehouseService {
+  async send(packaging: PackagingDTO): Promise<ResultAsync<any, any>> {
+    return errAsync("todo");
+  }
+}
